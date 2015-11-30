f6afeb765124cccaa396e0e902868d8cbfc6eeae
Theodore Baschak <theodore@ciscodude.net>
2015-11-30 09:50:13 -0600

    fix compilation error

--- src/TableLog.cc
+++ src/TableLog.cc
@@ -22,6 +22,7 @@
 // to the Free Software Foundation, Inc., 51 Franklin St,  Fifth Floor,
 // Boston, MA 02110-1301 USA.
 
+#include <stddef.h>
 #include <time.h>
 #include <sys/types.h>
 #include <dirent.h>
